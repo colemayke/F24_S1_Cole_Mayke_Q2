860ea755629bae80065443750ed79d305c21c223
Cole Mayke <cmayke@karrier.one>
2024-11-01 14:38:35 -0400

    Added error checking for division by 0 on a branch called NewBranch

--- src/com/jwetherell/algorithms/mathematics/Division.java
+++ src/com/jwetherell/algorithms/mathematics/Division.java
@@ -3,11 +3,13 @@ package com.jwetherell.algorithms.mathematics;
 public class Division {
 
     public static final long division(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divisor cannot be zero.");
         long result = ((long) a) / ((long) b);
         return result;
     }
 
     public static final long divisionUsingLoop(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divisor cannot be zero.");
         int absA = Math.abs(a);
         int absB = Math.abs(b);
 
@@ -22,6 +24,7 @@ public class Division {
     }
 
     public static final long divisionUsingRecursion(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divisor cannot be zero.");
         int absA = Math.abs(a);
         int absB = Math.abs(b);
 
@@ -38,6 +41,7 @@ public class Division {
     }
 
     public static final long divisionUsingMultiplication(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divisor cannot be zero.");
         int absA = Math.abs(a);
         int absB = Math.abs(b);
 
@@ -55,6 +59,7 @@ public class Division {
     }
 
     public static final long divisionUsingShift(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divisor cannot be zero.");
         int absA = Math.abs(a);
         int absB = Math.abs(b);
         int tempA, tempB, counter;
@@ -76,6 +81,7 @@ public class Division {
     }
 
     public static final long divisionUsingLogs(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divisor cannot be zero.");
         long absA = Math.abs(a);
         long absB = Math.abs(b);
         double logBase10A = Math.log10(absA);
